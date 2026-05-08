c1bb54ec651b05a20b6c79e8268b135a130b0ec8
qianyey204-lab <qianyey204@gmail.com>
2026-05-08 15:50:58 +0900

    Add files via upload

---
+++ Intro/songlist_BGM.js
@@ -0,0 +1,21 @@
+// songlist_bgm.js
+export const songListBGM = [
+  {
+    no: 1,
+    title: "「いちご白書」をもう一度",
+    artist: "バンバン",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/%E3%80%8Cいちご白書%E3%80%8Dをもう一度_バンバン.mp3"
+  },
+  {
+    no: 2,
+    title: "15の夜",
+    artist: "尾崎豊",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/15の夜_尾崎豊.mp3"
+  },
+  {
+    no: 3,
+    title: "22才の別れ",
+    artist: "風",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/22才の別れ_風.mp3"
+  }
+];
